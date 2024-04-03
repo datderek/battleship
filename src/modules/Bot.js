dc5c2cacf69b6d7ce751c774fe7973031e8d9da2
Derek Tran <tran.derek.d@gmail.com>
2024-04-03 15:25:10 -0700

    Refactor receiveFeedback for readability

--- src/modules/Bot.js
+++ src/modules/Bot.js
@@ -27,83 +27,101 @@ export default class Bot extends Player {
     }
   }
 
-  #addAdjacentTiles(row, col) {
-    this.attackQueue.push([row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]);
+  #setShipOrientation(row) {
+    if (row != this.shipEndpoints[0][0]) {
+      this.shipOrientation = 'vertical';
+    } else {
+      this.shipOrientation = 'horizontal';
+    }
+  }
+
+  #updateShipEndpoints(row, col) {
+    // Initial discovery of ship
+    if (this.shipEndpoints.length === 0) {
+      this.shipEndpoints.push([row, col], [row, col]);
+    }
+
+    // Subsequent endpoint updates
+    if (this.shipOrientation === 'vertical') {
+      const startRow = this.shipEndpoints[0][0];
+      
+      if (row < startRow) {
+        this.shipEndpoints[0] = [row, col];
+      } else {
+        this.shipEndpoints[1] = [row, col];
+      }
+    } else {
+      const startCol = this.shipEndpoints[0][1];
+
+      if (col < startCol) {
+        this.shipEndpoints[0] = [row, col];
+      } else {
+        this.shipEndpoints[1] = [row, col];
+      }
+    }
+  }
+
+  #targetNextEndpoint(row, col) {
+    if (this.shipOrientation === 'vertical') {
+      const startRow = this.shipEndpoints[0][0];
+
+      if (row === startRow) {
+        this.attackQueue.push([row - 1, col]);
+      } else {
+        this.attackQueue.push([row + 1 , col]);
+      }
+    } else {
+      const startCol = this.shipEndpoints[0][1];
+
+      if (col === startCol) { 
+        this.attackQueue.push([row, col - 1]);
+      } else { 
+        this.attackQueue.push([row, col + 1]);
+      }
+    }
+  }
+
+  #targetEndpoints(row, col) {
+    if (this.shipOrientation === 'vertical') {
+      const startRow = this.shipEndpoints[0][0];
+      const endRow = this.shipEndpoints[1][0];
+      this.attackQueue.push([startRow - 1, col], [endRow + 1, col]);
+    } else {
+      const startCol = this.shipEndpoints[0][1];
+      const endCol = this.shipEndpoints[1][1];
+      this.attackQueue.push([row, startCol - 1], [row, endCol + 1]);
+    }
   }
 
   /**
-   * Determines which mode we are in and what tiles the Bot should target
+   * Determines which mode we are in and what tiles the Bot should target next
+   * 
    * @param {} row 
    * @param {*} col 
    * @param {*} result 
    */
   receiveFeedback(row, col, result) {
     if (this.mode === 'searching' && result.message === 'Hit!') {
-      this.#addAdjacentTiles(row, col);
       this.mode = 'found';
-      this.shipEndpoints.push([row, col]);
+      this.attackQueue.push([row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]);
+      this.#updateShipEndpoints(row, col);
     } else if (this.mode === 'found' && result.message === 'Hit!') {
-      this.attackQueue = [];
       this.mode = 'target';
-      const prevRow = this.shipEndpoints[0][0];
-      const prevCol = this.shipEndpoints[0][1];
-
-      if (row != prevRow) {
-        this.shipOrientation = 'vertical';
-
-        if (row > prevRow) {
-          this.shipEndpoints.push([row, col]);
-        } else {
-          this.shipEndpoints.unshift([row, col]);
-        }
-
-        const startRow = this.shipEndpoints[0][0];
-        const endRow = this.shipEndpoints[1][0];
-        this.attackQueue.push([startRow - 1, col], [endRow + 1, col]);
-      } else {
-        this.shipOrientation = 'horizontal';
-
-        if (col > prevCol) {
-          this.shipEndpoints.push([row, col]);
-        } else {
-          this.shipEndpoints.unshift([row, col]);
-        }
-
-        const startCol = this.shipEndpoints[0][1];
-        const endCol = this.shipEndpoints[1][1];
-        this.attackQueue.push([row, startCol - 1], [row, endCol + 1]);
-      }
+      this.attackQueue = [];
+      this.#setShipOrientation(row);
+      this.#updateShipEndpoints(row, col);
+      this.#targetEndpoints(row, col)
     } else if (this.mode === 'target') {
       if (result.message === 'Hit!') {
-        if (this.shipOrientation === 'vertical') {
-          const startRow = this.shipEndpoints[0][0];
-
-          // Updates the endpoints
-          if (row > startRow) { // down
-            this.shipEndpoints[1] = [row, col];
-            this.attackQueue.push([row + 1, col]);
-          } else { // up
-            this.shipEndpoints[0] = [row, col];
-            this.attackQueue.push([row - 1 , col]);
-          }
-        } else {
-          const endCol = this.shipEndpoints[1][1];
-
-          if (col > endCol) { // right
-            this.shipEndpoints[1] = [row, col];
-            this.attackQueue.push([row, col + 1]);
-          } else { // left
-            this.shipEndpoints[0] = [row, col];
-            this.attackQueue.push([row, col - 1]);
-          }
-        }
+        this.#updateShipEndpoints(row, col);
+        this.#targetNextEndpoint(row, col);
       }
+    }
 
-      if (this.attackQueue.length === 0) {
-        this.mode = 'searching'
-        this.shipEndpoints = [];
-        this.shipOrientation = null;
-      }
+    if (this.attackQueue.length === 0) {
+      this.mode = 'searching'
+      this.shipEndpoints = [];
+      this.shipOrientation = null;
     }
   }
 
